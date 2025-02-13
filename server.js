8b734ebb27ca74ebb2e5d14c6f2177605edc590d
L1ttl3G4m3r <kaylee.hoek@hva.nl>
2025-02-13 14:05:39 +0100

    server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', home);
-app.get('/about', about);
+app.get("/", home);
+app.get("/about", about);
 app.listen(8000);
 
 function home(req, res) {
-    res.send('<h1>Hello</h1>');
+  res.send("<h1>Hello</h1>");
 }
 
 function about(req, res) {
-    res.send('<h1>About</h1>');
+  res.send("<h1>About</h1>");
 }
