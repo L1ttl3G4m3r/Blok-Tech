1ac170aac908e6f7c630b91c0daceef73a5f1f62
L1ttl3G4m3r <kaylee.hoek@hva.nl>
2025-02-13 17:37:49 +0100

    working static and templating

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use("/static", express.static("static"));
+
 app.get("/", home);
 app.get("/about", about);
 app.listen(8000);
@@ -12,3 +14,24 @@ function home(req, res) {
 function about(req, res) {
   res.send("<h1>About</h1>");
 }
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const users = ["kaylee", "tess"];
+
+app.get("/profile/:username", (req, res) => {
+  const username = req.params.username.toLowerCase();
+
+  if (!users.includes(username)) {
+    return res.status(404).send("Error 404: User not found");
+  }
+
+  let movie = {
+    title: "The Shawshank Redemption",
+    description: "Andy Dufresne is a young and ambitious banker sentenced to life in prison...",
+  };
+
+  // Render the EJS template correctly
+  res.render("detail", { data: movie, username });
+});
