fe8ec7da163d0c7a8f5c7155f3751384b169e28a
MickvdL <mln@kpnplanet.nl>
2025-03-31 20:54:09 +0200

    ja

--- static/script/script.js
+++ static/script/script.js
@@ -59,29 +59,6 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Formulier succesvol verzonden');
     });
 
-    // Tabs functionaliteit wordt geladen vanuit tabs.js
-    initializeTabs();
 });
 
-document.querySelectorAll('.add-to-collection').forEach(button => {
-  button.addEventListener('click', async () => {
-    const imageUrl = button.getAttribute('data-url');
 
-    try {
-      const response = await fetch('/add-to-collection', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ imageUrl })
-      });
-
-      if (response.ok) {
-        alert('Image added to your collection!');
-      } else {
-        alert('Failed to add image.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred.');
-    }
-  });
-});
